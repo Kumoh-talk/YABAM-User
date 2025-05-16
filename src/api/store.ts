9bd5ae83ddc7b3dfaa3b3ff1a5f76ed5207769e1
yunadada <ynson0216@naver.com>
2025-05-17 02:42:01 +0900

    feat: storeId 조회 API 정의

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,12 +1,12 @@
-import axios from "../lib/axiosInstance";
+import axiosInstance from "../lib/axiosInstance";
 import type { ApiResponse } from "../types/ApiResponse";
-import type { StoreListResponse } from "../types/Store";
+import type { StoreResponse, StoreListResponse } from "../types/Store";
 
 export const getStoreList = async (
   lastStoreId: number | null,
   size: number
 ): Promise<StoreListResponse> => {
-  const response = await axios.get<ApiResponse<StoreListResponse>>(
+  const response = await axiosInstance.get<ApiResponse<StoreListResponse>>(
     "/api/v1/stores",
     {
       params: {
@@ -18,4 +18,12 @@ export const getStoreList = async (
 
   console.log(response.data.data); // 응답 확인
   return response.data.data; // StoreListResponse 전체 반환
+};
+
+export const getStoreId = async (storeId: string): Promise<StoreResponse> => {
+  const response = await axiosInstance.get("/api/v1/store", {
+    params: storeId,
+  });
+
+  return response.data.data;
 };
