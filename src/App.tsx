a3cb38b90b3e2a6840d4345b6202b1d3f5decf55
wei756 <wei756fg@gmail.com>
2025-05-19 02:59:36 +0900

    fix: swiper css import 방식 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from "react";
 import "./App.css";
-import "swiper/css";
-import "swiper/css/pagination";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import StoreDetail from "./pages/StoreDetail/StoreDetail";
