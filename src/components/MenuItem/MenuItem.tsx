4e53672823ec2d5c28fbb510af69690308af043d
Kyxxn <gywns626@naver.com>
2025-05-19 10:35:54 +0900

    feat: 메뉴 사진 클릭 시 전체화면 팝업 구현

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import style from "./MenuItem.module.css";
 import Tag from "../Tag/Tag";
 import type { MenuInfoResponse } from "../../types/Menu";
@@ -8,23 +9,44 @@ type MenuItemProps = {
 };
 
 const MenuItem = ({ menu, onClick }: MenuItemProps) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const handleImageClick = () => {
+    if (menu.menuImageUrl) {
+      setIsPopupOpen(true);
+    }
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
-    <div
-      className={`${style.menuItem} ${menu.menuIsSoldOut ? style.soldOut : ""}`}
-      onClick={onClick}
-    >
-      <div className={style.img}>
-        <img src={menu.menuImageUrl} alt={menu.menuName} />
+    <>
+      <div
+        className={`${style.menuItem} ${menu.menuIsSoldOut ? style.soldOut : ""}`}
+        onClick={onClick}
+      >
+        <div className={style.img} onClick={handleImageClick}>
+          <img src={menu.menuImageUrl} alt={menu.menuName} />
+        </div>
+        <div className={style.contents}>
+          {menu.menuIsSoldOut && <p className={style.soldOutText}>품절</p>}
+          {menu.menuIsRecommended && <Tag content="주인장 추천!" />}
+          <h4>{menu.menuName}</h4>
+          <p className={style.description}>{menu.menuDescription}</p>
+          <p className={style.price}>{menu.menuPrice.toLocaleString()}원</p>
+        </div>
       </div>
-      <div className={style.contents}>
-           {menu.menuIsSoldOut && <p className={style.soldOutText}>품절</p>}
-        {menu.menuIsRecommended && <Tag content="주인장 추천!" />}
-        <h4>{menu.menuName}</h4>
-        <p className={style.description}>{menu.menuDescription}</p>
-        <p className={style.price}>{menu.menuPrice.toLocaleString()}원</p>
-     
-      </div>
-    </div>
+
+      {isPopupOpen && (
+        <div className={style.popupOverlay} onClick={closePopup}>
+          <div className={style.popupContent} onClick={(e) => e.stopPropagation()}>
+            <img src={menu.menuImageUrl} alt={menu.menuName} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
