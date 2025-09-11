ef65bd02f65ccbd7fea986e8d8cf120bfe949800
yunadada <ynson0216@naver.com>
2025-09-11 15:45:04 +0900

    refactor: 메뉴 목록 렌더링 구조 단순화

--- src/components/MenuList/MenuList.tsx
+++ src/components/MenuList/MenuList.tsx
@@ -14,20 +14,18 @@ const MenuList = ({ loading, menus, category, onMenuClick }: PropsType) => {
   return (
     <div className={style.menuCategory}>
       <h3>{category.menuCategoryName}</h3>
-      {loading ? (
-        // TODO: 배열 길이 몇 으로 해야할 지
-        Array.from({ length: 2 }).map((_, idx) => <MenuSkeleton key={idx} />)
-      ) : (
-        <div className={style.menuItems}>
-          {menus[category.menuCategoryId]?.map((menu) => (
-            <MenuItem
-              key={menu.menuId}
-              menu={menu}
-              {...(onMenuClick && { onClick: () => onMenuClick(menu) })}
-            />
-          ))}
-        </div>
-      )}
+      <div className={style.menuItems}>
+        {menus[category.menuCategoryId]?.map((menu) => (
+          <MenuItem
+            key={menu.menuId}
+            menu={menu}
+            {...(onMenuClick && { onClick: () => onMenuClick(menu) })}
+          />
+        ))}
+
+        {loading &&
+          Array.from({ length: 3 }).map((_, idx) => <MenuSkeleton key={idx} />)}
+      </div>
     </div>
   );
 };
